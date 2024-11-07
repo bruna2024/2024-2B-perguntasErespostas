43f849d7dc15e25ffd21b68f62b81437f682f764
bruna2024 <159148692+bruna2024@users.noreply.github.com>
2024-11-07 08:38:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+function criaCartao(Materia, Pergunta, Resposta){}
+
---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao (
+    "História",
+    "Por até quanto tempo durou a primeira revolução industrial?",
+    "Por volta de 1760 até 1850"
+)
+criaCartao(
+    "Geografia",
+    "Qual é a capital do Paraguay?",
+    "Assunção/Asunción"
+)
+criaCartao(
+    "Biologia",
+    "O'que é biogenese",
+    "Consiste na teoria atualmente aceita que explica a origem dos seres vivos, sendo estes apenas possíveis de existir a partir da reprodução de outros organismos vivos."
+)
+criaCartao(
+        "História",
+        "Qual foi o último continente a ser povoado pelos seres humanos?",
+        "América"
+)
+criaCartao(
+        "Geografia",
+        "Qual é a capital do Acre?",
+        "Rio Branco é a capital do estado do Acre, que se localiza na região Norte."
+)
+criaCartao(
+        "Biologia",
+        "O que é ecologia",
+        "A ecologia é a ciência que estuda as relações dos seres vivos entre si e deles com o meio ambiente."
+)
